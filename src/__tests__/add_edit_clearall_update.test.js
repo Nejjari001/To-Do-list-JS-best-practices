12d3ff382508f8e74794c2a53653507f2cadb6df
ReemMohamedAbdelfatah <reem.mohamed.abdelfatah@gmail.com>
2022-10-21 10:46:46 +0200

    Add Checkbox, Clear all completed functions and fixed linter errors

--- src/__tests__/add_edit_clearall_update.test.js
+++ src/__tests__/add_edit_clearall_update.test.js
@@ -3,6 +3,7 @@
  */
 import store from '../modules/store.js';
 import displayTodo from '../modules/function.js';
+import formClear from '../modules/cleartask.js';
 
 document.body.innerHTML = '<input type="text" name="task" class="text-field full" placeholder="Add to your list..." required> <ul class="task-list"> </ul> <a class="clear-btn" href="">Clear all completed</a>';
 
@@ -42,5 +43,29 @@ describe('Edit task', () => {
       description: 'Hey',
       completed: false,
     }]));
+  });
+});
+
+describe('Checkbox', () => {
+  test('Expect to change completed to tru after click', () => {
+    const checkBox = document.querySelector('.checkbox');
+    checkBox.click();
+    const locStorage = localStorage.getItem('todolist');
+
+    expect(locStorage).toEqual(JSON.stringify([{
+      index: 1,
+      description: 'Hello',
+      completed: true,
+    }]));
+  });
+});
+
+describe('Clear all completed', () => {
+  test('Should delete all the tasks marked as completed', () => {
+    const list = document.querySelectorAll('textarea');
+    store('Wooooorld');
+    formClear();
+    displayTodo();
+    expect(list).toHaveLength(1);
   });
 });
